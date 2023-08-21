336d0b5f715286f806ee81bc3f439e86ed07cf96
Arya Sunildath <aryadath@Aryas-MacBook-Air.local>
2023-08-21 20:18:52 +0100

    City API fetching data

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4172763446msh566127f7f173abep17e2cajsnc6893d5cece3',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
---
+++ src/components/search/search.js
@@ -0,0 +1,39 @@
+import { AsyncPaginate } from "react-select-async-paginate";
+import { useState } from 'react';
+import { GEO_API_URL,geoApiOptions  } from "../../api";
+
+const Search = ({onSearchChange}) => {
+
+    const [search, setSearch] = useState(null);
+
+    const loadOptions = (inputValue) => {
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+            geoApiOptions
+        )
+        .then((response) => response.json())
+        .then((response) => console.log(response))
+        .catch((err) => console.error(err));
+            
+        };
+
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    return(
+        <AsyncPaginate
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+        />
+    )
+}
+
+export default Search;
+
+//asyncPaginate= allows large information to be processed in smaller chunks
