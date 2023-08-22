12f12ccc6998d3cd0077b7fff06daa5a43730aa7
Arya <aryadath@gmail.com>
2023-08-22 21:18:23 +0100

    Completed current weather component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import './App.css';
-import Search from './components/search/search';
-import CurrentWeather from './components/current-weather/current-weather';
-import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
-import { useState } from 'react';
-
+import { useState } from "react";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+// import Forecast from "./components/forecast/forecast";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import "./App.css";
 
 function App() {
-  const [CurrentWeather, setCurrentWeather] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
 
-    const CurrentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&exclude={part}&appid=${WEATHER_API_KEY}`);
-    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&exclude={part}&appid=${WEATHER_API_KEY}`);
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
 
-    Promise.all([CurrentWeatherFetch,forecastFetch])
+    Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
-        const forecastResponse = await response[1].json();
+        const forcastResponse = await response[1].json();
 
-        setCurrentWeather({city: searchData.label , ...weatherResponse});
-        setForecast({city: searchData.label, ...forecastResponse});
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forcastResponse });
       })
-      .catch((err) => console.log(err));
-  }
+      .catch(console.log);
+  };
+
   return (
     <div className="container">
-      <Search onSearchChange={handleOnSearchChange}/>
-      <CurrentWeather/>
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {/* {forecast && <Forecast data={forecast} />} */}
     </div>
   );
 }
--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,24 +1,24 @@
 import "./current-weather.css"
 
 
-const CurrentWeather = () => {
+const CurrentWeather = ({data}) => {
     return (
         <div className="weather">
             <div className="top">
                 <div>
-                    <p className="city"> Belgrade</p>
-                    <p className="weather-description">Sunny</p>
+                    <p className="city"> {data.city}</p>
+                    <p className="weather-description">{data.weather[0].desctiption}</p>
                 </div>
-                <img alt="weather" className="weather-icon" src="icons/01d.png" />
+                <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
             </div>
             <div className="bottom">
-                <p className="temperature">18°C</p>
+                <p className="temperature">{Math.round(data.main.temp)}°C</p>
                 <div className="details">
                     <div className="paramater-row">
                         <span className="parameter-label">Details</span>
                     </div>
                     <div className="parameter-row">
-                        <span className="parameter-label">Feels like 22°C </span>
+                        <span className="parameter-label"></span>
                         <span className="parameter-value">
                             {Math.round(data.main.feels_like)}°C
                         </span>
