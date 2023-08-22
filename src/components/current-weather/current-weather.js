ec0e9b122ece4701877e0cc05a36fa81dd055e33
Arya <aryadath@gmail.com>
2023-08-22 19:30:19 +0100

    Static weather implemented

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -5,10 +5,38 @@ const CurrentWeather = () => {
     return (
         <div className="weather">
             <div className="top">
-                <p className="city"> Belgrade</p>
-                <p className="weather-description">Sunny</p>
+                <div>
+                    <p className="city"> Belgrade</p>
+                    <p className="weather-description">Sunny</p>
+                </div>
+                <img alt="weather" className="weather-icon" src="icons/01d.png" />
             </div>
-            <img alt="weather" className="weather-icon" src=""/>
+            <div className="bottom">
+                <p className="temperature">18°C</p>
+                <div className="details">
+                    <div className="paramater-row">
+                        <span className="parameter-label">Details</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Feels like 22°C </span>
+                        <span className="parameter-value">
+                            {Math.round(data.main.feels_like)}°C
+                        </span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Wind</span>
+                        <span className="parameter-value">{data.wind.speed} m/s</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Humidity</span>
+                        <span className="parameter-value">{data.main.humidity}%</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Pressure</span>
+                        <span className="parameter-value">{data.main.pressure} hPa</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
